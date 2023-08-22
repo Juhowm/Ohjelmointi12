a45b7487c4f561ae97fbf61751026208bd316024
Juho Mäkeläinen <juhowm@metropolia.fi>
2023-08-22 12:39:38 +0300

    Tehtävä 2.6 valmis

---
+++ Moduuli 2 Tehtävät/Tehtävä 2.6.py
@@ -0,0 +1,14 @@
+import random
+#ensimmäisen koodin luvut
+luvut11 = random.randint(0,9)
+luvut12 = random.randint(0,9)
+luvut13 = random.randint(0,9)
+
+#Toisen koodin luvut
+luvut21 = random.randint(1,6)
+luvut22 = random.randint(1,6)
+luvut23 = random.randint(1,6)
+luvut24 = random.randint(1,6)
+
+print(f"Kolminumeroinen koodi: {luvut11}{luvut12}{luvut13}")
+print(f"nelinumeroinen koodi: {luvut21}{luvut22}{luvut23}{luvut24}")
