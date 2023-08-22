3fbd9cea9fab070af5f1ba4b732492e75718d7e4
Juho Mäkeläinen <juhowm@metropolia.fi>
2023-08-22 13:29:50 +0300

    muistutus lisätty

--- Moduuli 2 Tehtävät/Tehtävä 2.5.py
+++ Moduuli 2 Tehtävät/Tehtävä 2.5.py
@@ -13,6 +13,7 @@ naulamassa = naula*13.3*32
 leivismassa = leivis*13.3*32*20
 tosimassa = luotimassa+naulamassa+leivismassa
 tosimassakilo = tosimassa//1000
+#kokeile modulo
 
 print(f"Massa modernisti on \n{tosimassakilo:.0f} kilogrammaa")
 print(f"ja {tosimassa-tosimassakilo*1000:.2f} grammaa")
