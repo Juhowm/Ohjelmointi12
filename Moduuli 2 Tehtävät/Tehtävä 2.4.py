a3de335050f2828b325692dab57bf8a357070d88
Juho Mäkeläinen <juhowm@metropolia.fi>
2023-08-22 11:04:49 +0300

    Tehtävä 2.4 valmis

---
+++ Moduuli 2 Tehtävät/Tehtävä 2.4.py
@@ -0,0 +1,9 @@
+luku1_kirj = input("Kirjoita ensimmäinen kokonaisluku: ")
+luku2_kirj = input("Kirjoita toinen kokonaisluku: ")
+luku3_kirj = input("kirjoita kolmas kokonaisluku: ")
+luku1 = float(luku1_kirj)
+luku2 = float(luku2_kirj)
+luku3 = float(luku3_kirj)
+lukusumma = luku1+luku2+luku3
+print(f"Näiden lukujen \nsumma on {lukusumma}\ntulo on {luku1*luku2*luku3}")
+print(f"keskiarvo on {lukusumma/3}")
