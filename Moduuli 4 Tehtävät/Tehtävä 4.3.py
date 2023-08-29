99bd75ad9450ad24cca783580d00e0bc3b2436f5
Juho Mäkeläinen <juhowm@metropolia.fi>
2023-08-29 15:32:15 +0300

    4.3 valmis ja 4.6 valmis?

--- Moduuli 4 Tehtävät/Tehtävä 4.3.py
+++ Moduuli 4 Tehtävät/Tehtävä 4.3.py
@@ -1,1 +1,8 @@
-
+luvut = []
+luku = input("anna luku: ")
+while luku != "":
+    luku = float(luku)
+    luvut.append(luku)
+    luku = input("anna luku: ")
+print(f"Pieni luku antamistasi oli {min(luvut)}"
+      f"\nja suurin oli {max(luvut)}")
--- Moduuli 4 Tehtävät/Tehtävä 4.6.py
+++ Moduuli 4 Tehtävät/Tehtävä 4.6.py
@@ -1,10 +1,14 @@
-import math
 import random
-
+pistesis = 0
+pistelrg = 0
 pistemr = int(input("Anna arvottavien pisteiden määrä"))
-if pistemr <= 0:
+if pistemr < 0:
     exit("negatiiviset luvut eivät kelpaa")
 while pistemr != 0:
     x = random.randrange(-1, 1)
     y = random.randrange(-1, 1)
+    if x**2+y**2 < 1:
+        pistesis += 1
     pistemr -= 1
+    pistelrg += 1
+print(f"{4*pistesis/pistelrg}")
