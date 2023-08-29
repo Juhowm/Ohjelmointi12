b674334736515147d6911e04dd00744ede47caf8
Juho Mäkeläinen <juhowm@metropolia.fi>
2023-08-29 09:57:19 +0300

    3.4 siistitty

--- Moduuli 3 Tehtävät/Tehtävä 3.3.py
+++ Moduuli 3 Tehtävät/Tehtävä 3.3.py
@@ -1,8 +1,7 @@
 sp = input("Oletko biologisesti mies vai nainen? ")
 if sp != "mies" and sp != "nainen":
     exit("kirjoitusvirhe tai odottamaton arvo sukupuolessa")
-else:
-    hg = float(input("Mikä on hemoglobiiniarvosi yksikkönä g/l? "))
+hg = float(input("Mikä on hemoglobiiniarvosi yksikkönä g/l? "))
 if sp == "mies" and hg > 195 or sp == "nainen" and hg > 175:
     print("Hemoglobiiniarvosi on korkea.")
 elif sp == "mies" and hg < 134 or sp == "nainen" and hg < 117:
