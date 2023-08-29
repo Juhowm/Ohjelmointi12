03125a93494b92b55d3a96fe15b08730229506e2
Juho Mäkeläinen <juhowm@metropolia.fi>
2023-08-29 09:58:51 +0300

    3.1 printtiä muutettu

--- Moduuli 3 Tehtävät/Tehtävä 3.1.py
+++ Moduuli 3 Tehtävät/Tehtävä 3.1.py
@@ -1,6 +1,6 @@
 koko = float(input("Mikä on kuhan pituus senttimetreinä? "))
 if koko >= 37:
-    print("Mukavan kokoinen kala!")
+    print("Hyvän kokoinen kala!")
 else:
     print("Liian pieni, laske takaisin järveen! "
           f"\nKuhan pitäisi olla noin {37-koko:.0f} senttiä pidempi!")
