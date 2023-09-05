714afd1b6bfdce17891f69d965ac65b440a95a30
Juho Mäkeläinen <juhowm@metropolia.fi>
2023-09-05 11:28:31 +0300

    5.3 tehty

--- Moduuli 5 Tehtävät/Tehtävä 5.3.py
+++ Moduuli 5 Tehtävät/Tehtävä 5.3.py
@@ -1,9 +1,8 @@
 luku = int(input("Anna kokonaisluku: "))
 onalk = True
 for num in range(2, luku):
-    if luku % num == 0:
+    if luku % num == 0 and onalk:
         onalk = False
         print("Antamasi luku ei ole alkuluku")
-        break
 if onalk:
     print("Antamasi luku on alkuluku")
